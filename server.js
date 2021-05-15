2143bf97da9cbdc6452224302c9871748b2cbd16
bsandeep137 <bsandeep137@gmail.com>
2021-05-15 12:59:49 +0530

    todo

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const { static } = require('express');
 const express = require('express');
- 
 const app = express();
+ app.use(express.json());
+ app.use(express.urlencoded({extended:true}))
+ var idx=1;
+ var data=[]
+ 
+
 app.use(express.static(__dirname+"/frontend"));
 app.get("/", function(req, res){
     res.send("Welcome to my Basic website");
@@ -34,6 +39,56 @@ app.get("/todo",function(req,res){
     let path=__dirname+"/frontend/html/todo.html"
     res.sendFile(path);
 })
+app.post('/add_todo',function(req,res){
+    console.log("entered into post")
+    var x=req.body;
+    x.id=idx;
+    idx++;
+    data.push(x)
+    res.send(JSON.stringify({id:x.id}));
+})
+
+app.get('/todo_list',function(req,res) {
+    res.send(data);
+    
+})
+app.get('/todo_list/:id',function(req,res) {
+    var Id=req.params.id;
+
+    res.send(data);
+    
+})
+app.put("/get_todo/:id",function(req,res) {
+    for( x of data){
+        if (x.id==req.params.id){
+            x.todo=req.body.todo;
+            x.completed=req.body.completed;
+        }
+
+    }
+    res.send("done updating")
+    
+})
+app.delete("/remove_todo/:id",function (req,res) {
+    data=data.filter((x)=>{return (x.id!=req.params.id)})
+    res.send("deleted")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 app.get("/register", function(req, res){
     let reps=__dirname+"/frontend/html/register.html"
